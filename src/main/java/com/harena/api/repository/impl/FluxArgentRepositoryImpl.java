36d43f67216ed752125bc43970724857153a9d73
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-07-27 16:23:17 +0300

    "Refactor: refactored FluxArgentRepositoryImpl"

--- src/main/java/com/harena/api/repository/impl/FluxArgentRepositoryImpl.java
+++ src/main/java/com/harena/api/repository/impl/FluxArgentRepositoryImpl.java
@@ -2,26 +2,18 @@ package com.harena.api.repository.impl;
 
 import com.harena.api.dto.json.FluxArgentDataJsonFile;
 import com.harena.api.repository.FluxArgentRepository;
+import com.harena.api.repository.utils.BaseRepository;
 import com.harena.api.repository.utils.ReadDataFromJsonFile;
 import com.harena.api.repository.utils.WriteDataToJsonFile;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class FluxArgentRepositoryImpl implements FluxArgentRepository {
-    private final ReadDataFromJsonFile<FluxArgentDataJsonFile> readDataFromJsonFile;
-    private final WriteDataToJsonFile<FluxArgentDataJsonFile> writeDataToJsonFile;
-    private final Path filePath;
-
+public class FluxArgentRepositoryImpl extends BaseRepository<FluxArgentDataJsonFile> implements FluxArgentRepository {
     public FluxArgentRepositoryImpl(ReadDataFromJsonFile<FluxArgentDataJsonFile> readDataFromJsonFile, WriteDataToJsonFile<FluxArgentDataJsonFile> writeDataToJsonFile, @Value("${path.to.file.fluxargent}") String filePath) {
-        this.readDataFromJsonFile = readDataFromJsonFile;
-        this.writeDataToJsonFile = writeDataToJsonFile;
-        this.filePath = Paths.get(filePath);
+        super(readDataFromJsonFile, writeDataToJsonFile, filePath);
     }
 
     public List<FluxArgentDataJsonFile> loadAllData() {
