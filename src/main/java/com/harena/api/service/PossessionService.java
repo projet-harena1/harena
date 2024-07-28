1e4be972c6110759208342ec7c67464cf7eb9b05
Hevitriniavo <hei.tantely@gmail.com>
2024-07-28 16:56:19 +0300

    chore: resolve conflit in class PossessionServiceImpl

--- src/main/java/com/harena/api/service/PossessionService.java
+++ src/main/java/com/harena/api/service/PossessionService.java
@@ -1,21 +1,10 @@
 package com.harena.api.service;
 
-import com.harena.api.dto.json.PossessionDataJsonFile;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public interface PossessionService {
-    List<PossessionDataJsonFile> findAllPossessions();
-
-    Optional<PossessionDataJsonFile> createPossession(PossessionDataJsonFile possession);
-
-    Optional<PossessionDataJsonFile> updatePossession(PossessionDataJsonFile possession);
-
-    boolean deletePossession(Long id);
-
-    int calculateFutureValue(Long id, LocalDate futureDate);
 }
